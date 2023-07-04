fde3ce762475966b82d7488b35fe4e7de4657436
Bwhitman33 <bwhitman33@gmail.com>
2023-07-04 13:22:39 -0400

    Fixed connection and server files. It works now

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,8 @@
-const mongoose = require("mongoose");
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/Social-Network-API",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+connect('mongodb://127.0.0.1:27017/SocialNetwork_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+module.exports = connection;
